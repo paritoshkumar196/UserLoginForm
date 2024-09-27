12d2eb5ef475f85aa234093c8ef587bc0c599e01
paritoshkumar196 <paritoshkumar196@gmail.com>
2024-09-27 11:41:46 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+const signin = document.getElementById("sign-in");
+const signup = document.getElementById("sign-up");
+const username = document.getElementById("username");
+const title=document.querySelector('.title');
+const text=document.querySelector('.text')
+
+signin.addEventListener("click", () => {
+  username.style.maxHeight = "0";
+  signin.classList.add("disable");
+  signup.classList.remove("disable");
+  title.innerHTML='Sign-In';
+  text.style.opacity='1';
+});
+
+signup.addEventListener("click", () => {
+  username.style.maxHeight = "60px";
+  signin.classList.remove("disable");
+  signup.classList.add("disable");
+  title.innerHTML='Sign-Up';
+  text.style.opacity='0';
+});
